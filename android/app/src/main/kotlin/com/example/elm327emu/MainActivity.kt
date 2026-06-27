8de8e19f53fb543aaa89ba18c4daafa36bf435b3
poropi <poropi44@gmail.com>
2026-06-27 22:25:28 +0900

    chore: Flutter プロジェクトスキャフォールド(android+macos)とテスト基盤

---
+++ android/app/src/main/kotlin/com/example/elm327emu/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.elm327emu
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ test/smoke_test.dart
@@ -0,0 +1,7 @@
+import 'package:test/test.dart';
+
+void main() {
+  test('smoke', () {
+    expect(1 + 1, 2);
+  });
+}
